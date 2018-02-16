6eda46468403188f3339d8b88ed1a82f3766bd3f
Bjorn Helgaas <bhelgaas@google.com>
2018-02-16 15:22:39 -0600

    PCI: Remove redundant probes for device reset support
    BugLink: https://bugs.launchpad.net/bugs/1800770
    
    We probe every device for whether it supports reset so we can tell whether
    to create a sysfs "reset" file for it.  We do that probe in
    pci_init_capabilities() during enumeration and save the result in
    dev->reset_fn.  The result doesn't depend on any other devices on the bus
    and shouldn't change after boot, so we don't need to do the probe again.
    
    Remove the pci_probe_reset_function() calls and rely on the dev->reset_fn
    we found during enumeration.  No functional change intended.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 204f4afa7ae50239c39adb13af42f5720fe7e9a5)
    Signed-off-by: AceLan Kao <acelan.kao@canonical.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -4570,9 +4570,8 @@ int pci_reset_function(struct pci_dev *dev)
 {
 	int rc;
 
-	rc = pci_probe_reset_function(dev);
-	if (rc)
-		return rc;
+	if (!dev->reset_fn)
+		return -ENOTTY;
 
 	pci_dev_lock(dev);
 	pci_dev_save_and_disable(dev);
@@ -4607,9 +4606,8 @@ int pci_reset_function_locked(struct pci_dev *dev)
 {
 	int rc;
 
-	rc = pci_probe_reset_function(dev);
-	if (rc)
-		return rc;
+	if (!dev->reset_fn)
+		return -ENOTTY;
 
 	pci_dev_save_and_disable(dev);
 
@@ -4631,9 +4629,8 @@ int pci_try_reset_function(struct pci_dev *dev)
 {
 	int rc;
 
-	rc = pci_probe_reset_function(dev);
-	if (rc)
-		return rc;
+	if (!dev->reset_fn)
+		return -ENOTTY;
 
 	if (!pci_dev_trylock(dev))
 		return -EAGAIN;
