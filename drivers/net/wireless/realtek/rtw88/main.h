0fc8568d403fe957dba2936a6589a27e964fd403
Ping-Ke Shih <pkshih@realtek.com>
2020-05-29 14:18:37 +0800

    UBUNTU: SAUCE: rtw88: fix EAPOL 4-way failure by finish IQK earlier
    BugLink: https://bugs.launchpad.net/bugs/1878147
    
    If we connect to an AP with WPA2 security, the IQK and the
    EAPOL 4-way handshake may be failed due to overlap, because
    driver does IQK right after assoc success.
    
    For 802.11n devices, they used to do IQK in driver that could
    requires more than 100ms to finished. During IQK, any TX/RX
    events are paused. So, if the EAPOL 4-way started before IQK
    is finished, the 1/4 and 2/4 could be dropped, then the AP
    will issue deauth with reason IEEE8021X_FAILED (23).
    
    To resolve this, move IQK routine into managed TX prepare,
    which is ieee80211_ops::mgd_prepare_tx() called before the
    managed frames (auth/assoc) are sent. This can make sure IQK
    is done before connection. While scanning, not to do IQK for
    each channel because it would take too long.
    
    For 802.11ac devices, they used to do IQK in firmware, and it
    takes less time to finish it, so we do not see EAPOL 4-way
    failure on them. But, it is still worth to move the IQK to
    mgd_prepare_tx().
    
    Fixes: f5df1a8b4376 ("rtw88: 8723d: Add 8723DE to Kconfig and Makefile")
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    (backported from
    https://lore.kernel.org/linux-wireless/20200518081444.7664-4-yhchuang@realtek.com/raw)
    Signed-off-by: You-Sheng Yang <vicamo.yang@canonical.com>
    Signed-off-by: AceLan Kao <acelan.kao@canonical.com>

--- drivers/net/wireless/realtek/rtw88/main.h
+++ drivers/net/wireless/realtek/rtw88/main.h
@@ -1716,6 +1716,8 @@ struct rtw_dev {
 	const struct sar_read *sar_read;
 	struct delayed_work sar_work;
 
+	bool need_rfk;
+
 	/* hci related data, must be last */
 	u8 priv[0] __aligned(sizeof(void *));
 };
@@ -1768,6 +1770,7 @@ void rtw_restore_reg(struct rtw_dev *rtwdev,
 		     struct rtw_backup_info *bckp, u32 num);
 void rtw_desc_to_mcsrate(u16 rate, u8 *mcs, u8 *nss);
 void rtw_set_channel(struct rtw_dev *rtwdev);
+void rtw_chip_prepare_tx(struct rtw_dev *rtwdev);
 void rtw_vif_port_config(struct rtw_dev *rtwdev, struct rtw_vif *rtwvif,
 			 u32 config);
 void rtw_tx_report_purge_timer(struct timer_list *t);
