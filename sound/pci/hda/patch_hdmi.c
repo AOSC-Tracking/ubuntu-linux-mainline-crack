b774749f5d79db2850478911a73455e35950295d
Hui Wang <hui.wang@canonical.com>
2020-09-15 11:51:32 +0800

    UBUNTU: SAUCE: ALSA: hda/hdmi - Check pin_eld->monitor_present
    BugLink: https://bugs.launchpad.net/bugs/1895603
    
    We introduced a patch edbc693b5bd3 from stable update, the upstream
    commit of that patch is 7f641e26a6df, that patch is for v5.2, if we
    backport it to v4.4 kernel, we need to do some change since the
    eld->monitor_present is not used in the v4.4 yet. So we change it to
    use pin_eld->monitor_present.
    
    Fixes: edbc693b5bd3 ("ALSA: hda/hdmi - Consider eld_valid when reporting jack event")
    Signed-off-by: Hui Wang <hui.wang@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: William Breathitt Gray <william.gray@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- sound/pci/hda/patch_hdmi.c
+++ sound/pci/hda/patch_hdmi.c
@@ -1638,7 +1638,7 @@ static bool hdmi_present_sense(struct hdmi_spec_per_pin *per_pin, int repoll)
 	jack = snd_hda_jack_tbl_get(codec, pin_nid);
 	if (jack) {
 		jack->block_report = !ret;
-		jack->pin_sense = (eld->monitor_present && eld->eld_valid) ?
+		jack->pin_sense = (pin_eld->monitor_present && eld->eld_valid) ?
 			AC_PINSENSE_PRESENCE : 0;
 	}
 	mutex_unlock(&per_pin->lock);
