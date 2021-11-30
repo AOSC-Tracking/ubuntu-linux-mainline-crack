9311e7f195b745f4ce183604ffd1e0ccb35145b7
Zhou Qingyang <zhou1615@umn.edu>
2021-12-01 00:50:39 +0800

    octeontx2-af: Fix a memleak bug in rvu_mbox_init()
    BugLink: https://bugs.launchpad.net/bugs/1956720
    
    commit e07a097b4986afb8f925d0bb32612e1d3e88ce15 upstream.
    
    In rvu_mbox_init(), mbox_regions is not freed or passed out
    under the switch-default region, which could lead to a memory leak.
    
    Fix this bug by changing 'return err' to 'goto free_regions'.
    
    This bug was found by a static analyzer. The analysis employs
    differential checking to identify inconsistent security operations
    (e.g., checks or kfrees) between two code paths and confirms that the
    inconsistent operations are not recovered in the current function or
    the callers, so they constitute bugs.
    
    Note that, as a bug found by static analysis, it can be a false
    positive or hard to trigger. Multiple researchers have cross-reviewed
    the bug.
    
    Builds with CONFIG_OCTEONTX2_AF=y show no new warnings,
    and our static analyzer no longer warns about this code.
    
    Fixes: 98c561116360 (“octeontx2-af: cn10k: Add mbox support for CN10K platform”)
    Signed-off-by: Zhou Qingyang <zhou1615@umn.edu>
    Link: https://lore.kernel.org/r/20211130165039.192426-1-zhou1615@umn.edu
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -2127,7 +2127,7 @@ static int rvu_mbox_init(struct rvu *rvu, struct mbox_wq_info *mw,
 			goto free_regions;
 		break;
 	default:
-		return err;
+		goto free_regions;
 	}
 
 	mw->mbox_wq = alloc_workqueue(name,
