23637daa074c3391331ee09281f6c4fc71c8b96b
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2021-12-02 10:53:31 +0100

    HID: sony: fix error path in probe
    BugLink: https://bugs.launchpad.net/bugs/1956720
    
    commit 7998193bccc1c6e1537c5f3880fd0d5b949ec9d1 upstream.
    
    When the setup of the GHL fails, we are not calling hid_hw_stop().
    This leads to the hidraw node not being released, meaning a crash
    whenever somebody attempts to open the file.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20211202095334.14399-2-benjamin.tissoires@redhat.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/hid/hid-sony.c
+++ drivers/hid/hid-sony.c
@@ -3025,22 +3025,27 @@ static int sony_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	 */
 	if (!(hdev->claimed & HID_CLAIMED_INPUT)) {
 		hid_err(hdev, "failed to claim input\n");
-		hid_hw_stop(hdev);
-		return -ENODEV;
+		ret = -ENODEV;
+		goto err;
 	}
 
 	if (sc->quirks & GHL_GUITAR_PS3WIIU) {
-		if (!hid_is_usb(hdev))
-			return -EINVAL;
+		if (!hid_is_usb(hdev)) {
+			ret = -EINVAL;
+			goto err;
+		}
+
 		usbdev = to_usb_device(sc->hdev->dev.parent->parent);
 
 		sc->ghl_urb = usb_alloc_urb(0, GFP_ATOMIC);
-		if (!sc->ghl_urb)
-			return -ENOMEM;
+		if (!sc->ghl_urb) {
+			ret = -ENOMEM;
+			goto err;
+		}
 		ret = ghl_init_urb(sc, usbdev);
 		if (ret) {
 			hid_err(hdev, "error preparing URB\n");
-			return ret;
+			goto err;
 		}
 
 		timer_setup(&sc->ghl_poke_timer, ghl_magic_poke, 0);
@@ -3048,6 +3053,10 @@ static int sony_probe(struct hid_device *hdev, const struct hid_device_id *id)
 			  jiffies + GHL_GUITAR_POKE_INTERVAL*HZ);
 	}
 
+	return ret;
+
+err:
+	hid_hw_stop(hdev);
 	return ret;
 }
 
