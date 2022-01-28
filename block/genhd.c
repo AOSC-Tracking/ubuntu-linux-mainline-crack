ff54f15384723d5af3c8688bf45bdd132c440cc9
Christoph Hellwig <hch@lst.de>
2022-01-28 11:25:57 +0800

    block: cleanup the lockdep handling in *alloc_disk
    BugLink: https://bugs.launchpad.net/bugs/1958850
    
    Pass the lockdep name to the low-level __blk_alloc_disk helper and
    hardcode the name for it given that the number of minors or node_id
    are not very useful information.  While this passes a pointless
    argument for non-lockdep builds that is not really an issue as
    disk allocation is a probe time only slow path.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20210816131910.615153-5-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 4dcc4874deb41a11ece9c6e8858385235463c1ac)
    Signed-off-by: Koba Ko <koba.ko@canonical.com>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- block/genhd.c
+++ block/genhd.c
@@ -1263,7 +1263,8 @@ dev_t blk_lookup_devt(const char *name, int partno)
 	return devt;
 }
 
-struct gendisk *__alloc_disk_node(int minors, int node_id)
+struct gendisk *__alloc_disk_node(int minors, int node_id,
+		struct lock_class_key *lkclass)
 {
 	struct gendisk *disk;
 
@@ -1291,7 +1292,10 @@ struct gendisk *__alloc_disk_node(int minors, int node_id)
 	disk_to_dev(disk)->type = &disk_type;
 	device_initialize(disk_to_dev(disk));
 	inc_diskseq(disk);
-
+	lockdep_init_map(&disk->lockdep_map, "(bio completion)", lkclass, 0);
+#ifdef CONFIG_BLOCK_HOLDER_DEPRECATED
+	INIT_LIST_HEAD(&disk->slave_bdevs);
+#endif
 	return disk;
 
 out_destroy_part_tbl:
@@ -1305,7 +1309,7 @@ out_free_disk:
 }
 EXPORT_SYMBOL(__alloc_disk_node);
 
-struct gendisk *__blk_alloc_disk(int node)
+struct gendisk *__blk_alloc_disk(int node, struct lock_class_key *lkclass)
 {
 	struct request_queue *q;
 	struct gendisk *disk;
@@ -1314,7 +1318,7 @@ struct gendisk *__blk_alloc_disk(int node)
 	if (!q)
 		return NULL;
 
-	disk = __alloc_disk_node(0, node);
+	disk = __alloc_disk_node(0, node, lkclass);
 	if (!disk) {
 		blk_cleanup_queue(q);
 		return NULL;
