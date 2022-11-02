2bb3baaaa39fcd15a0f0dcda0ea17e49d8dfdb32
Zhengchao Shao <shaozhengchao@huawei.com>
2022-11-02 10:06:10 +0800

    ipv6: fix WARNING in ip6_route_net_exit_late()
    BugLink: https://bugs.launchpad.net/bugs/2003596
    
    [ Upstream commit 768b3c745fe5789f2430bdab02f35a9ad1148d97 ]
    
    During the initialization of ip6_route_net_init_late(), if file
    ipv6_route or rt6_stats fails to be created, the initialization is
    successful by default. Therefore, the ipv6_route or rt6_stats file
    doesn't be found during the remove in ip6_route_net_exit_late(). It
    will cause WRNING.
    
    The following is the stack information:
    name 'rt6_stats'
    WARNING: CPU: 0 PID: 9 at fs/proc/generic.c:712 remove_proc_entry+0x389/0x460
    Modules linked in:
    Workqueue: netns cleanup_net
    RIP: 0010:remove_proc_entry+0x389/0x460
    PKRU: 55555554
    Call Trace:
    <TASK>
    ops_exit_list+0xb0/0x170
    cleanup_net+0x4ea/0xb00
    process_one_work+0x9bf/0x1710
    worker_thread+0x665/0x1080
    kthread+0x2e4/0x3a0
    ret_from_fork+0x1f/0x30
    </TASK>
    
    Fixes: cdb1876192db ("[NETNS][IPV6] route6 - create route6 proc files for the namespace")
    Signed-off-by: Zhengchao Shao <shaozhengchao@huawei.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Link: https://lore.kernel.org/r/20221102020610.351330-1-shaozhengchao@huawei.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>
    Signed-off-by: Luke Nowakowski-Krijger <luke.nowakowskikrijger@canonical.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -4755,8 +4755,15 @@ static void __net_exit ip6_route_net_exit(struct net *net)
 static int __net_init ip6_route_net_init_late(struct net *net)
 {
 #ifdef CONFIG_PROC_FS
-	proc_create("ipv6_route", 0, net->proc_net, &ipv6_route_proc_fops);
-	proc_create("rt6_stats", S_IRUGO, net->proc_net, &rt6_stats_seq_fops);
+	if (!proc_create("ipv6_route", 0, net->proc_net,
+			 &ipv6_route_proc_fops))
+		return -ENOMEM;
+
+	if (!proc_create("rt6_stats", S_IRUGO, net->proc_net,
+			 &rt6_stats_seq_fops)) {
+		remove_proc_entry("ipv6_route", net->proc_net);
+		return -ENOMEM;
+	}
 #endif
 	return 0;
 }
