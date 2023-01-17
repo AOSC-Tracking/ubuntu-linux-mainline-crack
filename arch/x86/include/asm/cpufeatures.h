dc83d0b320adec1d4bc2dbd35dfae8a23e60b0fa
Babu Moger <babu.moger@amd.com>
2023-01-17 13:51:06 +0800

    x86/cpufeatures: Add feature bit RDPRU on AMD
    BugLink: https://bugs.launchpad.net/bugs/1968681
    
    AMD Zen 2 introduces a new RDPRU instruction which is used to give
    access to some processor registers that are typically only accessible
    when the privilege level is zero.
    
    ECX is used as the implicit register to specify which register to read.
    RDPRU places the specified register’s value into EDX:EAX.
    
    For example, the RDPRU instruction can be used to read MPERF and APERF
    at CPL > 0.
    
    Add the feature bit so it is visible in /proc/cpuinfo.
    
    Details are available in the AMD64 Architecture Programmer’s Manual:
    https://www.amd.com/system/files/TechDocs/24594.pdf
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Aaron Lewis <aaronlewis@google.com>
    Cc: ak@linux.intel.com
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: "Peter Zijlstra (Intel)" <peterz@infradead.org>
    Cc: robert.hu@linux.intel.com
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Thomas Hellstrom <thellstrom@vmware.com>
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/20191007204839.5727.10803.stgit@localhost.localdomain
    (backported from commit 9d40b85bb46a99bc95dad3a07787da93b0a018e9)
    [PHLin: context adjustment to ignore WBNOINVD change]
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Luke Nowakowski-Krijger <luke.nowakowskikrijger@canonical.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -297,6 +297,7 @@
 #define X86_FEATURE_CLZERO		(13*32+ 0) /* CLZERO instruction */
 #define X86_FEATURE_IRPERF		(13*32+ 1) /* Instructions Retired Count */
 #define X86_FEATURE_XSAVEERPTR		(13*32+ 2) /* Always save/restore FP error pointers */
+#define X86_FEATURE_RDPRU		(13*32+ 4) /* Read processor register at user level */
 #define X86_FEATURE_AMD_IBPB		(13*32+12) /* "" Indirect Branch Prediction Barrier */
 #define X86_FEATURE_AMD_IBRS		(13*32+14) /* "" Indirect Branch Restricted Speculation */
 #define X86_FEATURE_AMD_STIBP		(13*32+15) /* "" Single Thread Indirect Branch Predictors */
